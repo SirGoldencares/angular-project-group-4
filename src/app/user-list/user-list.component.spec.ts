4f30a11110632d264d81a276d0f5c3fe301fbac6
Paul Afolabi <paulafolabi745@gmail.com>
2024-02-01 16:01:56 +0100

    unit test modified

--- src/app/user-list/user-list.component.spec.ts
+++ src/app/user-list/user-list.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserListComponent } from './user-list.component';
 
+//Test suite for UserListComponent
 describe('UserListComponent', () => {
   let component: UserListComponent;
   let fixture: ComponentFixture<UserListComponent>;
 
+  //This sets up the test environment before each test
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserListComponent]
@@ -17,6 +19,7 @@ describe('UserListComponent', () => {
     fixture.detectChanges();
   });
 
+  //This tests to ensure that the component is created successfully
   it('should create', () => {
     expect(component).toBeTruthy();
   });
