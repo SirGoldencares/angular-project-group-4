8b4fa6751c27c7ba0935a2a6049a57f7141d5334
Paul Afolabi <paulafolabi745@gmail.com>
2024-02-01 16:05:51 +0100

    unit test for AppComponent

--- src/app/userdetails/userdetails.component.spec.ts
+++ src/app/userdetails/userdetails.component.spec.ts
@@ -2,10 +2,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserdetailsComponent } from './userdetails.component';
 
+//Test suite for UserdetailsComponent
 describe('UserdetailsComponent', () => {
   let component: UserdetailsComponent;
   let fixture: ComponentFixture<UserdetailsComponent>;
 
+  //This sets up the test environment before each test
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [UserdetailsComponent]
@@ -17,6 +19,7 @@ describe('UserdetailsComponent', () => {
     fixture.detectChanges();
   });
 
+  //This tests to ensure that the component is created successfully
   it('should create', () => {
     expect(component).toBeTruthy();
   });
