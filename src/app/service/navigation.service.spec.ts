5d5d73384a9f914d02d39bb6afd8408f97bc8354
Paul Afolabi <paulafolabi745@gmail.com>
2024-02-01 15:59:54 +0100

    unit tests

--- src/app/service/navigation.service.spec.ts
+++ src/app/service/navigation.service.spec.ts
@@ -2,14 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { NavigationService } from './navigation.service';
 
+//Unit test suite for NavigationService
 describe('NavigationService', () => {
   let service: NavigationService;
 
+  //This sets up test environment before each test
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(NavigationService);
   });
 
+  // This tests to ensure that the service is created successfully
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
--- src/app/service/user.service.spec.ts
+++ src/app/service/user.service.spec.ts
@@ -2,14 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
+//Unit test suite for UserService
 describe('UserService', () => {
   let service: UserService;
 
+  //This sets up the test environment before each test
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(UserService);
   });
 
+  //This tests to ensure that the service is created successfully
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
